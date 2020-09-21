fac92709628c31842e37f82fa8898906511a43e8
João Victor <jlopes6@gmail.com>
2020-09-20 22:01:42 -0300

    Login user ee hash password

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../model/User');
-const {registerValidation} = require('../validation');
+const bcrypt = require('bcryptjs');
+const {registerValidation, loginValidation} = require('../validation');
 
 router.post('/register', async (req, res) => {
 
@@ -12,18 +13,37 @@ router.post('/register', async (req, res) => {
     const emailExist = await User.findOne({email: req.body.email});
     if (emailExist) return res.status(400).send('Email já existe');
 
+    // Codificar a senha
+    const salt = await bcrypt.genSalt(10);
+    const hashSenha = await bcrypt.hash(req.body.senha, salt);
+
     // Criar novo usuario
     const user = new User({
         nome: req.body.nome,
-        senha: req.body.senha,
+        senha: hashSenha,
         email: req.body.email
     });
     try {
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send({user: user._id});
     } catch (err) {
         res.status(400).send(err);
     }
+});
+
+router.post('/login', async (req, res) => {
+    // Valido os campos
+    const valido = await loginValidation(req.body);
+    if (valido.error) return res.status(400).send(valido.detalhes);
+    //Checar se o email já esta cadastrado
+    const user = await User.findOne({email: req.body.email});
+    if (!user) return res.status(400).send('Email não foi encontrado');
+
+    const validPass = await bcrypt.compare(req.body.senha, user.senha);
+    if (!validPass) return res.status(400).send('Senha invalida');
+
+    res.send('Tudo ok!');
+
 });
 
 module.exports = router;
